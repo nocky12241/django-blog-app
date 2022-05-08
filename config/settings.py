02367fb4b1bbeebdf9e91bb90b2c36158e7e3aa9
nocky12241 <71242416.t12241so@gmail.com>
2022-05-08 23:30:20 +0900

    fix bugs

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','nocky0875/pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1','nocky0875.pythonanywhere.com']
 
 
 # Application definition
